00151a8a6220540355fd6dfbb7d5a4790fce908c
Peter Harvey <harvey@actenum.com>
2024-11-16 13:39:20 +1100

    Updated to Parcel 2.13.0 with longer sequence of repro steps.

--- bug.sh
+++ bug.sh
@@ -2,30 +2,59 @@
 
 echo
 echo "Removing all caches"
+echo
 rm -rf node_modules
-rm -rf .parcel_cache
+rm -rf .parcel-cache
 rm -rf dist
-
 echo
 echo "Installing dependencies"
+echo
 npm install
 
 echo
-echo "Doing a first build, which should PASS"
+echo "-------------------------------------------------"
+echo
+echo "Doing build #1, which should PASS because the code is correct"
+echo
+
 npm exec parcel build
 
+echo
+echo "-------------------------------------------------"
 echo
 echo "Breaking an import statement"
 sed -i 's_lodash-es_./other.js_' src/main.js
-
+echo "Doing build #2, which should FAIL because the code is incorrect"
 echo
-echo "Doing a second build, which should FAIL"
+
 npm exec parcel build
 
 echo
+echo "-------------------------------------------------"
+echo
 echo "Unbreaking the import"
 sed -i 's_./other.js_lodash-es_' src/main.js
+echo "Doing build #3, which should PASS because the code is correct"
+echo
+
+npm exec parcel build
 
 echo
-echo "Doing a third build, which should PASS but will FAIL"
+echo "-------------------------------------------------"
+echo
+echo "Breaking an import statement"
+sed -i 's_lodash-es_./other.js_' src/main.js
+echo "Doing build #4, which should FAIL because the code is incorrect"
+echo
+
+npm exec parcel build
+
+echo
+echo "-------------------------------------------------"
+echo
+echo "Unbreaking the import"
+sed -i 's_./other.js_lodash-es_' src/main.js
+echo "Doing build #5, which should PASS but will FAIL"
+echo
+
 npm exec parcel build
